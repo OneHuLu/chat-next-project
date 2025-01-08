1a6ff7c4b8b05f2432eff2aaeb1fd2691f7625a9
panghulu <panghulu678@163.com>
2025-01-08 14:43:56 +0800

    refactor(LoginComponent) 使用 SCSS 优化样式，调整输入框和按钮的大小

--- app/components/Login/LoginComponent.tsx
+++ app/components/Login/LoginComponent.tsx
@@ -1,5 +1,6 @@
 import { Button, Input } from "antd";
 import AuthCardComponent from "../AuthCard/AuthCardComponent";
+import styles from "./login.module.scss";
 interface LoginProps {
   isLogin?: boolean;
   setLogin?: (isLogin: boolean) => void;
@@ -7,18 +8,20 @@ interface LoginProps {
 export default function LoginComponent(props: LoginProps) {
   return (
     <AuthCardComponent isChange={props.isLogin ?? false}>
-      <h1>Login</h1>
-      <Input placeholder="请输入邮箱" />
-      <Input placeholder="请输入密码" />
-      <div>
-        <Input placeholder="请输入验证码" />
-        <Button>获取验证码</Button>
-      </div>
-      <div>
-        尚未注册，前往
-        <span onClick={() => props.setLogin?.(false)}>
-          <i>注册</i>
-        </span>
+      <div className={styles.login}>
+        <h1>Login</h1>
+        <Input placeholder="请输入邮箱" size="large" />
+        <Input placeholder="请输入密码" size="large" />
+        <div className={styles.get_code}>
+          <Input placeholder="请输入验证码" size="large" />
+          <Button size="large" className={styles.btn}>获取验证码</Button>
+        </div>
+        <div>
+          尚未注册，前往
+          <span onClick={() => props.setLogin?.(false)}>
+            <i>注册</i>
+          </span>
+        </div>
       </div>
     </AuthCardComponent>
   );
