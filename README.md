f36607b3999fc5331d30b586331bee2ac8fcec36
panghulu <panghulu678@163.com>
2025-01-14 17:03:02 +0800

    feat(AuthCard) 重构 AuthCard 组件样式，移除 SCSS 文件并添加 Tailwind CSS 支持，更新 README 和配置文件

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## 技术选型确定
+
 前端框架: Next.js
 编程语言: TypeScript
--- app/components/AuthCard/AuthCardComponent.tsx
+++ app/components/AuthCard/AuthCardComponent.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styles from './authCard.module.scss';
 interface AuthCardSlotProps {
   children: React.ReactNode;
   isChange: boolean;
@@ -8,9 +7,6 @@ export default function AuthCardComponent({
   children,
   isChange,
 }: Readonly<AuthCardSlotProps>) {
-  return (
-    <div className={`${styles.card} ${isChange ? styles.show : styles.hide}`}>
-      {children}
-    </div>
-  );
+  const cardClassNames = `absolute w-11/12 max-w-[600px] min-w-[260px] h-4/5 max-h-[400px] p-5 bg-white rounded-lg shadow-lg text-center transform transition-transform duration-700 ease-in-out ${isChange ? 'translate-x-0 opacity-100 z-20' : 'translate-x-full opacity-0 z-10'}`;
+  return <div className={cardClassNames}>{children}</div>;
 }
